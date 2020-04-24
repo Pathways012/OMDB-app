98941e4e0dc20128de261bb6ecc48043ba54f1a7
Pathways012 <64288063+Pathways012@users.noreply.github.com>
2020-04-24 18:39:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mobilefinalproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobilefinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    void loginFunction(String  username, String password)
+    {
+        return;
+    }
+}
