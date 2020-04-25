469542f99dbe514d7748203a058c8fa48bc7e35c
Pathways012 <64288063+Pathways012@users.noreply.github.com>
2020-04-25 17:16:48 -0400

    Implemented SearchActivity and had it sent its message to a textView in SearchResultsActivity

--- app/src/main/java/com/example/mobilefinalproject/SearchActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/SearchActivity.java
@@ -2,13 +2,38 @@ package com.example.mobilefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
+    EditText userInput;
+    Button searchResultsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchResultsButton = findViewById(R.id.searchResultsButton);
+        userInput = findViewById(R.id.userInput);
+
+        searchResultsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.searchResultsButton:
+                        Intent intent = new Intent(SearchActivity.this, SearchResultsActivity.class);
+                        String message = userInput.getText().toString();
+                        intent.putExtra("extraMessage", message);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/mobilefinalproject/SearchResultsActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/SearchResultsActivity.java
@@ -2,13 +2,24 @@ package com.example.mobilefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SearchResultsActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        textView = findViewById(R.id.textView);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getExtras().getString("extraMessage");
+
+        textView.setText(message);
     }
 }
